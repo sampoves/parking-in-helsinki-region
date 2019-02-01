72108132c2a51972f9bf176f4269ad7e7ba83a7e
Sampo Vesanen <31888573+sampoves@users.noreply.github.com>
2019-02-01 03:54:22 +0200

    my listener is faulty with radio button and option dropdown menus

--- park_survey.js
+++ park_survey.js
@@ -5,47 +5,42 @@
  */
 
 // TSEKKAA MIKÄ ADDMARKER() TUOLLA LOPUSSA ON EKALLA RIVIL
-var popupContent = '<form role="form" id="form" enctype="multipart/form-data" class = "form-horizontal" onsubmit="addMarker()">'+
+var popupContent = '<form name="myform" role="form" id="form" enctype="multipart/form-data" class = "form-horizontal" onsubmit="addMarker()">'+
     //datetime
     '<div class="form-group">'+
-        '<label class="control-label col-sm-5"><strong>Pysäköintisi päivämäärä ja kellonaika</strong></label>'+
-        '<input type="datetime-local" placeholder="Required" id="date" name="date" class="form-control"/>'+ 
+        '<label class="control-label col-sm-5"><strong>Pysäköintisi päivämäärä ja kellonaika<br></strong></label>'+
+        '<input type="datetime-local" placeholder="Required" id="datetime" name="datetime" class="form-control"/>'+ 
     '</div>'+
 
     //time to search parking
     '<div class="form-group">'+
-        '<label class="control-label col-sm-5"><strong>Kuinka pitkään etsit pysäköintipaikkaa (minuuteissa)?: </strong></label>'+
-        '<input type="number" min="0" max="120" placeholder="Required" class="form-control" id="parktime" name="parktime">'+ 
+        '<label class="control-label col-sm-5"><strong>Kuinka pitkään etsit pysäköintipaikkaa (minuuteissa)?:<br></strong></label>'+
+        '<input type="number" min="0" max="120" placeholder="Required" class="form-control" id="parktime" name="parktime">'+
     '</div>'+
 
     //likert familiarity of parking area
     '<div class="row">'+
         '<label class="control-label col-sm-5"><strong>Kuinka usein olet pysäköinyt autosi tälle alueelle?</strong></label>'+
-        '<div class="small-2 column text-right">'+
-            '<p>strongly agree</p>'+
-        '</div>'+
         '<div class="small-3 column">'+
-            '<ul class="likert">'+
-                '<li><input value="1" name="likert" type="radio">Erittäin usein</li>'+
-                '<li><input value="2" name="likert" type="radio">Usein</li>'+
-                '<li><input value="3" name="likert" type="radio">Toisinaan</li>'+
-                '<li><input value="4" name="likert" type="radio">Harvoin</li>'+
-                '<li><input value="5" name="likert" type="radio">En koskaan</li>'+
+            '<ul class="likert" onclick="myfunction()">'+
+                '<li><input value="1" id="likert" name="likert" type="radio">Erittäin usein</li>'+
+                '<li><input value="2" id="likert" name="likert" type="radio">Usein</li>'+
+                '<li><input value="3" id="likert" name="likert" type="radio">Toisinaan</li>'+
+                '<li><input value="4" id="likert" name="likert" type="radio">Harvoin</li>'+
+                '<li><input value="5" id="likert" name="likert" type="radio">En koskaan</li>'+
             '</ul>'+
-        '</div>'+
-        '<div class="small-2 column end">'+
-            '<p>strongly disagree</p>'+
         '</div>'+
     '</div>'+
 
     //type of parking spot
     '<div class="form-group">'+
-        '<label class="control-label col-sm-5"><strong>Minkätyyppinen pysäköintipaikka oli kyseessä?</strong></label>'+
-        '<select class="form-control" placeholder="Required" id="parkspottype" name="parkspottype">'+
-            '<option value="1">Kadunvarsipaikka</option>'+
-            '<option value="2">Pysäköintialue (parkkipaikka)</option>'+
-            '<option value="3">Pysäköintihalli</option>'+
-            '<option value="4">Muu</option>'+
+        '<label for="parkspot">Minkätyyppinen pysäköintipaikka oli kyseessä?<br></label>'+
+        '<select id="parkspot" name="parkspot" onclick="myfunction()">'+
+            '<option disabled selected value> -- select an option -- </option>'+
+            '<option value="3" name="parkspot" label="Kadunvarsipaikka"></option>'+
+            '<option value="2" name="parkspot" label="Pysäköintialue (parkkipaikka)"></option>'+
+            '<option value="3" name="parkspot" label="Pysäköintihalli"></option>'+
+            '<option value="4" name="parkspot" label="Muu"></option>'+
         '</select>'+ 
     '</div>'+
     

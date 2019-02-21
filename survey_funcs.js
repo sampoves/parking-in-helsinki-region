679acccd3b8e8a25ea517b4da6b5d6ebb297d42d
Sampo Vesanen <31888573+sampoves@users.noreply.github.com>
2019-02-21 23:20:18 +0200

    submit results button can be disabled manually
    also, langbutton will not break sendbutton inactivity

--- survey_funcs.js
+++ survey_funcs.js
@@ -124,13 +124,21 @@ function createjscssfile(filename, filetype){
  
 //replacejscssfile("oldscript.js", "newscript.js", "js");
 function replacejscssfile(oldfilename, newfilename, filetype){
-    var targetelement=(filetype === "js")? "script" : (filetype === "css")? "link" : "none"; //determine element type to create nodelist using
+    var targetelement = (filetype === "js")? "script" : (filetype === "css")? "link" : "none"; //determine element type to create nodelist using
     var targetattr = (filetype === "js")? "src" : (filetype === "css")? "href" : "none"; //determine corresponding attribute to test for
     var allsuspects = document.getElementsByTagName(targetelement);
-    for (var i=allsuspects.length; i >= 0; i--){ //search backwards within nodelist for matching elements to remove
+    for (var i = allsuspects.length; i >= 0; i--){ //search backwards within nodelist for matching elements to remove
         if (allsuspects[i] && allsuspects[i].getAttribute(targetattr) !== null && allsuspects[i].getAttribute(targetattr).indexOf(oldfilename) !== -1){
             var newelement = createjscssfile(newfilename, filetype);
             allsuspects[i].parentNode.replaceChild(newelement, allsuspects[i]);
         }
     }
+}
+
+function testElementActive(elemName, varName){
+    if(varName === true){
+        return elemName.update();
+    } else if (varName === false){
+        return elemName.disable();
+    }
 }

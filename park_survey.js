8f09862f7a77b6762a4cc3baa4c1d0d788a5f3d8
Sampo Vesanen <31888573+sampoves@users.noreply.github.com>
2019-02-26 23:11:38 +0200

    info dialog translated
    translations are outdated; from survey v1

--- park_survey.js
+++ park_survey.js
@@ -9,19 +9,19 @@ var popupContent =
         '<form name="myform" role="form" id="form" enctype="multipart/form-data" class = "form-horizontal" onsubmit="addMarker()">'+
     //datetime
     '<div class="form-group">'+
-        '<label class="control-label col-sm-5"><strong>Pysäköintisi päivämäärä ja kellonaika<br></strong></label>'+
+        '<label class="control-label col-sm-5"><strong tkey="lang_datetime">Pysäköintisi päivämäärä ja kellonaika<br></strong></label>'+
         '<input type="datetime-local" placeholder="Required" id="datetime" name="datetime" class="form-control"/>'+ 
     '</div>'+
 
     //time to search parking
     '<div class="form-group">'+
-        '<label class="control-label col-sm-5"><strong>Kuinka pitkään etsit pysäköintipaikkaa (minuuteissa)?:<br></strong></label>'+
+        '<label class="control-label col-sm-5"><strong tkey="lang_search">Kuinka pitkään etsit pysäköintipaikkaa (minuuteissa)?:<br></strong></label>'+
         '<input type="number" min="0" max="120" placeholder="Required" class="form-control" id="parktime" name="parktime">'+
     '</div>'+
 
     //likert familiarity of parking area
     '<div class="form-group">'+
-        '<label class="control-label col-sm-5"><strong>Kuinka usein olet pysäköinyt autosi tälle alueelle?</strong></label>'+
+        '<label class="control-label col-sm-5"><strong tkey="lang_likert">Kuinka usein olet pysäköinyt autosi tälle alueelle?</strong></label>'+
         '<div class="likertcolumn">'+
             '<ul class="likert" id="likert">'+
                 //<label></label> makes possible to click on radio button texts as well as the radio button
@@ -36,14 +36,14 @@ var popupContent =
 
     //type of parking spot
     '<div class="form-group">'+
-        '<label class="control-label col-sm-5"><strong>Minkätyyppinen pysäköintipaikka oli kyseessä?</strong><br></label>'+
+        '<label class="control-label col-sm-5"><strong tkey="lang_parkspot">Minkätyyppinen pysäköintipaikka oli kyseessä?</strong><br></label>'+
         '<div class="parkspotcolumn">'+
             '<select id="parkspot" name="parkspot" onchange="onParkspotChange(event)">'+
-                '<li><option value="0" name="parkspot" selected disabled value> -- select an option -- </option></li>'+
-                '<li><option value="1" name="parkspot">Kadunvarsipaikka</option></li>'+
-                '<li><option value="2" name="parkspot">Pysäköintialue (parkkipaikka)</option></li>'+
-                '<li><option value="3" name="parkspot">Pysäköintihalli</option></li>'+
-                '<li><option value="4" name="parkspot">Muu</option></li>'+
+                '<li><option value="0" name="parkspot" tkey="parkspotd" selected disabled value>-- select an option --</option></li>'+
+                '<li><option value="1" name="parkspot" tkey="parkspot1">Kadunvarsipaikka</option></li>'+
+                '<li><option value="2" name="parkspot" tkey="parkspot2">Pysäköintialue (parkkipaikka)</option></li>'+
+                '<li><option value="3" name="parkspot" tkey="parkspot3">Pysäköintihalli</option></li>'+
+                '<li><option value="4" name="parkspot" tkey="parkspot4">Muu</option></li>'+
             '</select>'+ 
         '</div>'+
     '</div>'+

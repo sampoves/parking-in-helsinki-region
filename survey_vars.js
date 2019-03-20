232e683844412782dbc7978d5b8827918b74d568
Sampo Vesanen <31888573+sampoves@users.noreply.github.com>
2019-03-20 19:44:45 +0200

    looks like styling function when creating geojson layer is buggy
    one feature defaults to the blue color no matter what

--- survey_vars.js
+++ survey_vars.js
@@ -75,38 +75,41 @@ var voyagerOnlyLabels = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertil
 
 
 //MARKER VARIABLES
+//
+//these are obsolete now that survey uses polygons
+//
 //incomplete icon
-var incompleteIcon = L.icon({
-    iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png",
-    iconSize: [25, 41],
-    shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
-    shadowAnchor: [8, 20],
-    shadowSize: [25, 18],
-    iconAnchor: [12, 40],
-    popupAnchor: [0, -41]
-});
-
-//complete icon
-var completeIcon = L.icon({
-    iconUrl: "https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png",
-    iconSize: [25, 41],
-    shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
-    shadowAnchor: [8, 20],
-    shadowSize: [25, 18],
-    iconAnchor: [12, 40],
-    popupAnchor: [0, -41]
-});
-
-//submitted icon
-var submittedIcon = L.icon({
-    iconUrl: "https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-grey.png",
-    iconSize: [25, 41],
-    shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
-    shadowAnchor: [8, 20],
-    shadowSize: [25, 18],
-    iconAnchor: [12, 40],
-    popupAnchor: [0, -41]
-});
+//var incompleteIcon = L.icon({
+//    iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png",
+//    iconSize: [25, 41],
+//    shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
+//    shadowAnchor: [8, 20],
+//    shadowSize: [25, 18],
+//    iconAnchor: [12, 40],
+//    popupAnchor: [0, -41]
+//});
+//
+////complete icon
+//var completeIcon = L.icon({
+//    iconUrl: "https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png",
+//    iconSize: [25, 41],
+//    shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
+//    shadowAnchor: [8, 20],
+//    shadowSize: [25, 18],
+//    iconAnchor: [12, 40],
+//    popupAnchor: [0, -41]
+//});
+//
+////submitted icon
+//var submittedIcon = L.icon({
+//    iconUrl: "https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-grey.png",
+//    iconSize: [25, 41],
+//    shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
+//    shadowAnchor: [8, 20],
+//    shadowSize: [25, 18],
+//    iconAnchor: [12, 40],
+//    popupAnchor: [0, -41]
+//});
 
 
 
@@ -122,12 +125,22 @@ var stylePostal = {
 var stylePostalDark = {
     "color": "#082759",
     "weight": 3,
-    "opacity": 0.4,
+    "opacity": 0.3,
     "fillOpacity": 0
 };
 
+// for postaldissolve
 var styleArea = {
     "color": "#ff7800",
+    "weight": 3,
+    "opacity": 0.5,
+    "fillColor": '#0000FF',
+    "fillOpacity": 0
+};
+
+// for postaldissolve
+var styleAreaDark = {
+    "color": "#000000",
     "weight": 3,
     "opacity": 0.5,
     "fillColor": '#0000FF',

672ce0f5bf06ab408aa7928255b8ce41246602ad
Sampo Vesanen <31888573+sampoves@users.noreply.github.com>
2019-03-23 16:33:35 +0200

    fixin

--- testnew.php
+++ testnew.php
@@ -34,7 +34,7 @@ if (!filter_var($sanitizedLikert, FILTER_VALIDATE_INT, array("options" => array(
     
     // Error found given variable isn't allowed
     $response['status'] = 'error';
-    $response['message'] = 'Invalid value for likert.';
+    $response['message'] = sprintf('Invalid value for likert. Value given %s', $sanitizedLikert);
     
     // Echo JSON and exit script
     exit(json_encode($response));
@@ -43,14 +43,14 @@ if (!filter_var($sanitizedLikert, FILTER_VALIDATE_INT, array("options" => array(
 // Validate input for parkspot
 if (!filter_var($sanitizedParkspot, FILTER_VALIDATE_INT, array("options" => array("min_range"=>1, "max_range"=>4)))) {
     $response['status'] = 'error';
-    $response['message'] = 'Invalid value for parkspot.';
+    $response['message'] = sprintf('Invalid value for parkspot. Value given %s', $sanitizedParkspot);
     exit(json_encode($response));
 }
 
 // Validate input for parktime
 if (!filter_var($sanitizedParktime, FILTER_VALIDATE_INT, array("options" => array("min_range"=>0, "max_range"=>99)))) {   
     $response['status'] = 'error';
-    $response['message'] = 'Invalid value for parktime.';
+    $response['message'] = sprintf('Invalid value for parktime. Value given %s', $sanitizedParktime);
     exit(json_encode($response));
 }
 

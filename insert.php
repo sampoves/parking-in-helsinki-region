734d9ab38664d4dcab1247b112051681b453f6e1
Sampo Vesanen <31888573+sampoves@users.noreply.github.com>
2019-03-28 22:59:34 +0200

    visit counter works
    mysql approach is success

--- insert.php
+++ insert.php
@@ -13,34 +13,33 @@ function visitCounter($connection, $ip) {
 	$ipQuery = "SELECT * FROM visitors WHERE ip = '" .$ip. "';";
 	$ipResult = mysqli_query($connection, $ipQuery);
 	$num = mysqli_num_rows($ipResult);
-	
+
 	if($num === 0) {
 		// this ip address is new to the site; insert IP and first time info
 		$insertNewIpQuery = "INSERT INTO visitors(ip, ts_first, ts_latest, count) VALUES ('" .$ip. "', NOW(), NOW(), 1);";
+
 		//mysqli_query($connection, $insertNewIpQuery);
-		if ($conn->query($insertNewIpQuery) === TRUE) {
+		if ($connection->query($insertNewIpQuery) === TRUE) {
 			echo("Visit counter success. A new visitor! Welcome!");
 		} else {
-			exit(sprintf("Visit counter failure. Connection error: %s", $conn->error));
+			echo(sprintf("Visit counter failure. Connection error: %s", $connection->error));
 		}
-		
 	} else {
 		// this ip address has been to the site; update timestamp latest and view count
 		$addToCountQuery = "UPDATE visitors SET count = count + 1 WHERE ip = '" .$ip. "';";
 		$latestVisitQuery = "UPDATE visitors SET ts_latest = NOW() WHERE ip = '" .$ip. "';";
-		
-		//mysqli_query($connection, $addToCountQuery);
-		if ($conn->query($addToCountQuery) === TRUE) {
-			echo("Visit counter count+1 success. A returning visitor! Welcome back!");
-		} else {
-			exit(sprintf("Visit counter count+1 failure. Connection error: %s", $conn->error));
-		}
 
-		//mysqli_query($connection, $latestVisitQuery);		
-		if ($conn->query($latestVisitQuery) === TRUE) {
-			echo("Visit counter latest visit success.");
+		//mysqli_query($connection, $addToCountQuery);
+		if ($connection->query($addToCountQuery) === TRUE) {
+			echo("Visit counter 'count+1' success. A returning visitor! Welcome back!");
 		} else {
-			exit(sprintf("Visit counter latest visit failure. Connection error: %s", $conn->error));
+			echo(sprintf("Visit counter 'count+1' failure. Connection error: %s", $connection->error));
+		}
+		//mysqli_query($connection, $latestVisitQuery);		
+		if ($connection->query($latestVisitQuery) === TRUE) {
+			echo("\r\nVisit counter 'latest visit' update success.");
+		} else {
+			echo(sprintf("\r\nVisit counter 'latest visit' update failure. Connection error: %s", $connection->error));
 		}
 	}
 }

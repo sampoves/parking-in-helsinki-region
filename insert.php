e3a5d34622aada003379649b242699f118cee98e
Sampo Vesanen <31888573+sampoves@users.noreply.github.com>
2019-03-28 21:46:24 +0200

    visitor counter not yet working

--- insert.php
+++ insert.php
@@ -10,11 +10,11 @@ function insertMySQL($timestamp, $sessionid, $zipcode, $likert, $parkspot, $park
 // visit counter mysql function
 // https://technopoints.co.in/unique-visit-counter-ip-address-php/
 function visitCounter($connection, $ip) {
-	$ipQuery = "SELECT * FROM visitors WHERE ip = '" .$ip. "'";
+	$ipQuery = "SELECT * FROM visitors WHERE ip = '" .$ip. "';";
 	$ipResult = mysqli_query($connection, $ipQuery);
 	$num = mysqli_num_rows($ipResult);
 	
-	if($num == 0) {
+	if($num === 0) {
 		// this ip address is new to the site; insert IP and first time info
 		$insertNewIpQuery = "INSERT INTO visitors(ip, ts_first, ts_latest, count) VALUES ('" .$ip. "', NOW(), NOW(), 1);";
 		//mysqli_query($connection, $insertNewIpQuery);
--- unique_visitors.php
+++ unique_visitors.php
@@ -1,15 +1,17 @@
 <?php
-
-//Database variables
+// Check for empty POST and redirect to survey if request empty
+//if(empty($_POST)) {
+//	header('location:index.html');
+//}
+//Database variables and queries
 include_once("./../config.php");
-
+include_once("./../insert.php");
 // Check connection
 if ($conn->connect_error) {
 	//$response['status'] = 'error';
 	//$response['message'] = sprintf('Could not connect to database: %s', $conn->connect_error);
 	exit("Connection failed. Can't update visitor counter.");
 }
-
 // Perform operations
 $ip = getUserIpAddr();
 visitCounter($conn, $ip);
